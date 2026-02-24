24e60088c6747545acef3fc55567a94cfd6c2e7e
Prashantmahor <110157014+Prashantmahor@users.noreply.github.com>
2026-02-24 14:02:05 +0530

    Update Database.py

--- Database.py
+++ Database.py
@@ -1,19 +1,40 @@
-#Database Connection Settings
-import psycopg2
+# #Database Connection Settings
+# import psycopg2
 
+
+# def connection():
+#     con = psycopg2.connect(
+#         host="localhost",
+#         database="ecommerce",
+#         user="postgres",
+#         password="dev",
+#         port="5432"
+#     )
+
+#     if con:
+#         print("Connention successful")
+#     else:
+#         print("Connection failed")
+#     return con
+
+# conn = connection()
+
+import psycopg2
+import streamlit as st
 
 def connection():
-    con = psycopg2.connect(
-        host="localhost",
-        database="ecommerce",
-        user="postgres",
-        password="dev",
-        port="5432"
-    )
+    try:
+        con = psycopg2.connect(
+            host=st.secrets["DB_HOST"],
+            database=st.secrets["DB_NAME"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"],
+            port=st.secrets["DB_PORT"],
+            sslmode="require"
+        )
+        return con
+    except Exception as e:
+        st.error(f"Database connection failed: {e}")
+        return None
 
-    if con:
-        print("Connention successful")
-    else:
-        print("Connection failed")
-    return con
 conn = connection()
